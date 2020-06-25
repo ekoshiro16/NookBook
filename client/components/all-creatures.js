d451d6e201c27090fde5efba04d971968745eaab
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-25 13:39:47 -0700

    Front end: Made edits to allCreatures component

--- client/components/all-creatures.js
+++ client/components/all-creatures.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 export class AllCreatures extends React.Component {
-  componentDidMount() {
-    this.props.getCreatures()
-  }
+  // componentDidMount() {
+  //   this.props.getCreatures()
+  // }
 
   render() {
     return (
@@ -15,32 +15,44 @@ export class AllCreatures extends React.Component {
           </p>
         </div>
 
-        {/* Fish View */}
-        <div className="fishView">
-          <div className="fishTitle">
-            <a href="/creatures/fish">All Fish</a>
+        <div className="allCreaturesView">
+          {/* Fish View */}
+          <div className="fishView">
+            <div className="fishTitle">
+              <a href="/creatures/fish">All Fish</a>
+            </div>
+            <div className="fishImg">
+              <a href="/creatures/fish">
+                <img
+                  src="https://dodo.ac/np/images/4/4b/Black_Bass_NH.png"
+                  className="responsive-img"
+                  height="400px"
+                  width="400px"
+                  float="left"
+                  object-fit="cover"
+                />
+              </a>
+            </div>
           </div>
-          <a href="/creatures/fish">
-            <img
-              src="https://dodo.ac/np/images/4/4b/Black_Bass_NH.png"
-              className="responsive-img"
-              height="50%"
-            />
-          </a>
-        </div>
 
-        {/* Bug View */}
-        <div className="bugView">
-          <div className="bugTitle">
-            <a href="/creatures/bugs">All Bugs</a>
+          {/* Bug View */}
+          <div className="bugView">
+            <div className="bugTitle">
+              <a href="/creatures/bugs">All Bugs</a>
+            </div>
+            <div className="bugImg">
+              <a href="/creatures/bugs">
+                <img
+                  src="https://dodo.ac/np/images/thumb/f/f1/Emperor_Butterfly_NH.png/180px-Emperor_Butterfly_NH.png"
+                  className="responsive-img"
+                  height="400px"
+                  width="400px"
+                  float="left"
+                  object-fit="cover"
+                />
+              </a>
+            </div>
           </div>
-          <a href="/creatures/bugs">
-            <img
-              src="https://dodo.ac/np/images/thumb/f/f1/Emperor_Butterfly_NH.png/180px-Emperor_Butterfly_NH.png"
-              className="responsive-img"
-              height="50%"
-            />
-          </a>
         </div>
       </div>
     )

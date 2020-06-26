05c9bd259987109170f8c3736d2e48c490ced1fe
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-26 03:34:24 -0700

    Front end: Edited allBugs component to display one row of bugs

--- client/components/creatures/all-bugs.js
+++ client/components/creatures/all-bugs.js
@@ -14,60 +14,150 @@ export class AllBugs extends React.Component {
         <div className="row">
           <p className="flow-text">Bugs (Critterpedia View)</p>
         </div>
-        <div className="row">
-          <div id="all-bugs-view">
-            <div className="column">
-              {/* Common Butterfly */}
-              <div className="previewBox">
-                <img
-                  src="https://acnhapi.com/v1/icons/bugs/1"
-                  className="responsive-img"
-                  height="75px"
-                  width="75px"
-                />
-              </div>
+        <div className="column">
+          {/* Common Butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/1"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
 
-              {/* Yellow Butterfly */}
-              <div className="previewBox">
-                <img
-                  src="https://acnhapi.com/v1/icons/bugs/2"
-                  className="responsive-img"
-                  height="75px"
-                  width="75px"
-                />
-              </div>
+          {/* Paper Kite Butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/6"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
 
-              {/* Tiger Butterfly */}
-              <div className="previewBox">
-                <img
-                  src="https://acnhapi.com/v1/icons/bugs/3"
-                  className="responsive-img"
-                  height="75px"
-                  width="75px"
-                />
-              </div>
+          {/* Rajah Brooke's birdwing */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/11"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
 
-              {/* Peacock Butterfly */}
-              <div className="previewBox">
-                <img
-                  src="https://acnhapi.com/v1/icons/bugs/4"
-                  className="responsive-img"
-                  height="75px"
-                  width="75px"
-                />
-              </div>
+          {/* Long locust */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/16"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
 
-              {/* Common Bluebottle */}
-              <div className="previewBox">
-                <img
-                  src="https://acnhapi.com/v1/icons/bugs/5"
-                  className="responsive-img"
-                  height="75px"
-                  width="75px"
-                />
-              </div>
-            </div>
-          </div>
+          {/* Bell cricket */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/21"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Brown cicada */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/26"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Cicada shell */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/31"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Firefly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/36"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Stingbug */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/41"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Violin beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/46"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Earth-boring dung beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/51"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Miyama stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/56"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Giraffe stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/61"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Walking stick */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/66"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Wharf roach */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/71"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Pill bug */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/76"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+        </div>
+        <div className="column">
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/1"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
+
+          {/* Yellow Butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/2"
+            className="responsive-img"
+            height="75px"
+            width="75px"
+          />
         </div>
       </div>
     )

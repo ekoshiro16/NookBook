4356fee528e7a02a874dd9ccba4ecbd458dbb912
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-26 17:21:35 -0700

    Front end: Edited img sizing css and added all bug images to allBugs component for grid view

--- client/components/creatures/all-bugs.js
+++ client/components/creatures/all-bugs.js
@@ -19,144 +19,484 @@ export class AllBugs extends React.Component {
           <img
             src="https://acnhapi.com/v1/icons/bugs/1"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Paper Kite Butterfly */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/6"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Rajah Brooke's birdwing */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/11"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Long locust */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/16"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Bell cricket */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/21"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Brown cicada */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/26"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Cicada shell */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/31"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Firefly */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/36"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Stingbug */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/41"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Violin beetle */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/46"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Earth-boring dung beetle */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/51"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Miyama stag */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/56"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Giraffe stag */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/61"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Walking stick */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/66"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Wharf roach */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/71"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
 
           {/* Pill bug */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/76"
             className="responsive-img"
-            height="75px"
-            width="75px"
           />
         </div>
         <div className="column">
-          <img
-            src="https://acnhapi.com/v1/icons/bugs/1"
-            className="responsive-img"
-            height="75px"
-            width="75px"
-          />
-
           {/* Yellow Butterfly */}
           <img
             src="https://acnhapi.com/v1/icons/bugs/2"
             className="responsive-img"
-            height="75px"
-            width="75px"
+          />
+
+          {/* Great purple emperor */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/7"
+            className="responsive-img"
+          />
+
+          {/* Queen Alexandra's birdwing */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/12"
+            className="responsive-img"
+          />
+
+          {/* Migratory locust */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/17"
+            className="responsive-img"
+          />
+
+          {/* Mantis */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/22"
+            className="responsive-img"
+          />
+
+          {/* Robust cicada */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/27"
+            className="responsive-img"
+          />
+
+          {/* Red dragonfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/32"
+            className="responsive-img"
+          />
+
+          {/* Mole cricket */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/37"
+            className="responsive-img"
+          />
+
+          {/* Man-faced stink bug */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/42"
+            className="responsive-img"
+          />
+
+          {/* Citrus long-horned beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/47"
+            className="responsive-img"
+          />
+
+          {/* Scarab beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/52"
+            className="responsive-img"
+          />
+
+          {/* Giant Stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/57"
+            className="responsive-img"
+          />
+
+          {/* Horned dysnastid */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/62"
+            className="responsive-img"
+          />
+
+          {/* Walking leaf */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/67"
+            className="responsive-img"
+          />
+
+          {/* Fly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/72"
+            className="responsive-img"
+          />
+
+          {/* Centipede */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/77"
+            className="responsive-img"
+          />
+        </div>
+        <div className="column">
+          {/* Tiger butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/3"
+            className="responsive-img"
+          />
+
+          {/* Monarch butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/8"
+            className="responsive-img"
+          />
+
+          {/* Moth */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/13"
+            className="responsive-img"
+          />
+
+          {/* Rice grasshopper */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/18"
+            className="responsive-img"
+          />
+
+          {/* Orchid mantis */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/23"
+            className="responsive-img"
+          />
+
+          {/* Giant cicada */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/28"
+            className="responsive-img"
+          />
+
+          {/* Darner dragonfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/33"
+            className="responsive-img"
+          />
+
+          {/* Pondskater */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/38"
+            className="responsive-img"
+          />
+
+          {/* Ladybug */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/43"
+            className="responsive-img"
+          />
+
+          {/* Rosalia batesi beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/48"
+            className="responsive-img"
+          />
+
+          {/* Drone beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/53"
+            className="responsive-img"
+          />
+
+          {/* Rainbow stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/58"
+            className="responsive-img"
+          />
+
+          {/* Horned atlas */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/63"
+            className="responsive-img"
+          />
+
+          {/* Bagworm */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/68"
+            className="responsive-img"
+          />
+
+          {/* Mosquito */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/73"
+            className="responsive-img"
+          />
+
+          {/* Spider */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/78"
+            className="responsive-img"
+          />
+        </div>
+        <div className="column">
+          {/* Peacock butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/4"
+            className="responsive-img"
+          />
+
+          {/* Emperor butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/9"
+            className="responsive-img"
+          />
+
+          {/* Atlas moth */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/14"
+            className="responsive-img"
+          />
+
+          {/* Grasshopper */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/19"
+            className="responsive-img"
+          />
+
+          {/* Honeybee */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/24"
+            className="responsive-img"
+          />
+
+          {/* Walker cicada */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/29"
+            className="responsive-img"
+          />
+
+          {/* Banded dragonfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/34"
+            className="responsive-img"
+          />
+
+          {/* Diving beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/39"
+            className="responsive-img"
+          />
+
+          {/* Tiger beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/44"
+            className="responsive-img"
+          />
+
+          {/* Blue weevil beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/49"
+            className="responsive-img"
+          />
+
+          {/* Goliath beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/54"
+            className="responsive-img"
+          />
+
+          {/* Cyclommatus stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/59"
+            className="responsive-img"
+          />
+
+          {/* Horned elephant */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/64"
+            className="responsive-img"
+          />
+
+          {/* Ant */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/69"
+            className="responsive-img"
+          />
+
+          {/* Flea */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/74"
+            className="responsive-img"
+          />
+
+          {/* Tarantula */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/79"
+            className="responsive-img"
+          />
+        </div>
+        <div className="column">
+          {/* Common bluebottle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/5"
+            className="responsive-img"
+          />
+
+          {/* Agrias butterfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/10"
+            className="responsive-img"
+          />
+
+          {/* Madagascan sunset moth */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/15"
+            className="responsive-img"
+          />
+
+          {/* Cricket */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/20"
+            className="responsive-img"
+          />
+
+          {/* Wasp */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/25"
+            className="responsive-img"
+          />
+
+          {/* Evening cicada */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/30"
+            className="responsive-img"
+          />
+
+          {/* Damselfly */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/35"
+            className="responsive-img"
+          />
+
+          {/* Giant water bug */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/40"
+            className="responsive-img"
+          />
+
+          {/* Jewel beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/45"
+            className="responsive-img"
+          />
+
+          {/* Dung beetle */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/50"
+            className="responsive-img"
+          />
+
+          {/* Saw stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/55"
+            className="responsive-img"
+          />
+
+          {/* Golden stag */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/60"
+            className="responsive-img"
+          />
+
+          {/* Horned hercules */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/65"
+            className="responsive-img"
+          />
+
+          {/* Hermit crab */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/70"
+            className="responsive-img"
+          />
+
+          {/* Snail */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/75"
+            className="responsive-img"
+          />
+
+          {/* Scorpion */}
+          <img
+            src="https://acnhapi.com/v1/icons/bugs/80"
+            className="responsive-img"
           />
         </div>
       </div>

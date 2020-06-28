81bc36188982d1f66142785e7537dba439c87ff8
Elle Oshiro <ekoshiro16@gmail.com>
2020-06-27 22:28:54 -0700

    Back end: Added routes for fish

--- server/api/fish.js
+++ server/api/fish.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
 const {Fish} = require('../db/models')
 
+// displays selected fish by id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const selectedFish = await Fish.findByPk(req.params.id)
+    if (selectedFish) {
+      res.status(200).json(selectedFish)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
 // displays all fish directory page
 router.get('/', async (req, res, next) => {
   try {
--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 router.use('/users', require('./users'))
 router.use('/creatures', require('./creatures'))
 router.use('/creatures/fish', require('./fish'))
+router.use('/creatures/fish/:id', require('./fish'))
 router.use('/creatures/bugs', require('./bugs'))
 router.use('/creatures/bugs/:id', require('./bugs'))
 
